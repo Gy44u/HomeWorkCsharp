56288194d9d3fc8bae851fac33c53208a4f911b6
Anton <buldakovanton@yandex.ru>
2023-07-10 14:12:46 +0700

    commit for push

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿/*Task 1. Напишите программу, которая на вход принемает два числа
+и выдирает большее число.
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) 
+  Console.WriteLine("max= " + num1);
+  else Console.WriteLine("max= " + num2);*/
+
+/*Task 2. Напишите программу, которая принимает на ввод
+// три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input a first number: ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the third nnumber: ");
+    int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) 
+{
+    max = num2;
+}
+if (num3 > max) 
+{
+    max = num3;
+}
+Console.WriteLine("Max nubber is: " + max);*/
+
+/*Task 3. Напишите программу, которая на вход принемает число 
+и выдаёт, является ли число чётным (делиться ли оно на два 
+без остатка).
+
+Console.Write("Input number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 % 2 == 0)
+     
+     Console.WriteLine("Yes");
+
+else 
+     Console.WriteLine("No" );*/
+
+/*Task 4. Напишите программу, которая на входе принемает
+число(N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Iptut a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while (current <= num1)
+   { if  (current % 2 == 0) 
+    Console.Write (current + " ");
+    current++;
+    }
+return (current);*/
+
+/*Console.Write("Iptut a number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int current = 2;
+while (current <= num1)
+    {Console.Write (current + " ");
+    current = current + 2;
+    }
+return (current);*/
+
