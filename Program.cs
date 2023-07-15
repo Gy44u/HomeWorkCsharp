6d0bc495e285403f88d5c6da8a3d6740c0db2e2b
Булдаков Антон <136948728+Gy44u@users.noreply.github.com>
2023-07-15 09:32:56 +0700

    Update Program.cs
    добавлено DZ3

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ while (current <= num1)
     }
 return (current);*/
 
-__________________________________________________________________________________________
+________________________________DZ2___________________________________________________
 
 // Task 5. Задача 10: Напишите программу, которая принимает на вход трёхзначное число
 // и на выходе показывает вторую цифру этого числа.
@@ -131,3 +131,73 @@ string WorkHoliday(int a)
 int dayNumber = ReadInt("Введите число от 1 до 7: ");
 Console.WriteLine(WorkHoliday(dayNumber));*/
 
+              
+________________________________DZ3________________________________________________________
+
+//Task 8.Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+/*void Quad (int num)
+{
+    int count =1;
+    while(count<=num)
+    {
+        Console.WriteLine($"{count} - {count*count*count}");
+        count++;
+    }
+}
+Console.WriteLine("");
+Console.Write("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Quad(num);*/
+
+/* Task 9. Напишите программу, которая принимает на вход 
+координаты двух точек и находит расстояние между ними 
+в 3D пространстве. 
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+
+double Distans( int x1, int x2, int y1, int y2, int z1, int z2)
+
+{
+    double result = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+    return result;
+ 
+}
+Console.Write("Input x1 ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y1 ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input z1 ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input x2 ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input y2 ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input z2 ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Distans(x1,x2,y1,y2,z1,z2));*/
+
+/* Task 10. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+
+ bool isPal(int n)
+  {
+      int r=0,nn=n;
+      while (n>0)
+      {
+          r=r*10+n%10;
+          n=n/10;
+      }
+      return r==nn;
+  }
+    int n;  
+    Console.Write("n=");
+    n=Convert.ToInt32(Console.ReadLine());
+    if (isPal(n))
+       Console.WriteLine("Палиндром");
+    else   
+       Console.WriteLine("Не палиндром");
+*/
